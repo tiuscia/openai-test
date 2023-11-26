76cda32e22ab38f5c325aebafb033f7bffed4551
tiuscia <tiuscia@gmail.com>
2023-11-26 16:17:27 +0000

    added keyword generator

---
+++ README.md
@@ -0,0 +1,7 @@
+Playing around with openai v4 (https://github.com/openai/openai-node) and node.js
+
+### How to run
+
+```sh
+npm app
+```
--- controllers/openAiController.js
+++ controllers/openAiController.js
@@ -1,7 +1,7 @@
 const openai = require("../config/openAiConfig")
 
 const generateChatCompletion = async (title) => {
-  const chatCompletion = await openai.chat.completions.create({
+  const chatCompletionDescription = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     messages: [
       {
@@ -11,7 +11,18 @@ const generateChatCompletion = async (title) => {
     ],
     max_tokens: 100,
   })
-  console.log(chatCompletion.choices[0].message)
+  console.log(chatCompletionDescription.choices[0].message)
+  const chatCompletionKw = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content: `Come up 10 seo keywords, for a webpage called ${title}`,
+      },
+    ],
+    max_tokens: 100,
+  })
+  console.log(chatCompletionKw.choices[0].message)
 }
 
 module.exports = { generateChatCompletion }
